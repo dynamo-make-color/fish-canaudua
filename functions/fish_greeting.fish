6fc41fbd695f44aba66889faf29401ce4758108f
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-15 17:03:57 +0700

    Basic greeting with custom art file in ~/.config/fish/art

---
+++ functions/fish_greeting.fish
@@ -0,0 +1,10 @@
+function fish_greeting -d "What's up, fish?"
+  test -f ~/.config/fish/art; and cat ~/.config/fish/art
+  set_color $fish_color_autosuggestion
+  uname -nmsr
+
+  command -s uptime >/dev/null
+  and command uptime
+
+  set_color normal
+end
