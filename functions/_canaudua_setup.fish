47f2defa1826c5110a4aa872ba93050e01e1e181
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-22 00:42:31 +0700

    Move to setting folder

--- functions/_canaudua_setup.fish
+++ functions/_canaudua_setup.fish
@@ -2,6 +2,7 @@ function _canaudua_setup
   set fish_bind_mode $fish_bind_mode
   set -g canaudua_left_prompt_$fish_pid (_canaudua_prompt)
 
-  set -q canaudua_left_items; or set -U canaudua_left_items pwd
-  set -q canaudua_right_items; or set -U canaudua_right_items time
+  for setting in items
+    printf 'set -U canaudua_%s\n' (cat ~/.config/fish/functions/setting/$setting) | source
+  end
 end
