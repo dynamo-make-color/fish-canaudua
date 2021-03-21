85a7c9631e8cade1dc8547f55d1b6330bbcd9b19
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-22 00:42:51 +0700

    Powerline section of prompt

--- functions/_canaudua_setup.fish
+++ functions/_canaudua_setup.fish
@@ -2,7 +2,7 @@ function _canaudua_setup
   set fish_bind_mode $fish_bind_mode
   set -g canaudua_left_prompt_$fish_pid (_canaudua_prompt)
 
-  for setting in items
+  for setting in items powerline color
     printf 'set -U canaudua_%s\n' (cat ~/.config/fish/functions/setting/$setting) | source
   end
 end
--- functions/_canaudua_side_prompt.fish
+++ functions/_canaudua_side_prompt.fish
@@ -1,9 +1,55 @@
-function _canaudua_side_prompt
-  set -l list_items_var canaudua_$argv[1]_items
+function _canaudua_side_prompt -a side
+  set -l list_items_var canaudua_"$side"_items
+  set -l prev_item start
 
+  # Loop over all items of side prompt
   for item in $$list_items_var
+    # Check output of item
     set -l output (_canaudua_item_$item)
     test -z output; and continue
-    printf '%s' "$output "
+
+    # Get background color of item
+    set -l bg_color_var canaudua_"$item"_bg
+    set -l bg_color $$bg_color_var
+
+    if test "$prev_item" = "start"
+      # Print head of prompt if is right prompt
+      if test "$side" = "right"
+        set_color $bg_color -b normal
+        printf '%s' $canaudua_right_separator_diff
+      end
+    else
+      if test "$bg_color" = "$prev_bg_color"
+        # Print separator with previous color if previous item and current item has same background
+        set -l separator_var canaudua_"$side"_separator_same
+        printf '%s' $$separator_var
+      else
+        if test "$side" = "right"
+          set_color $bg_color -b $prev_bg_color
+        else
+          set_color $prev_bg_color -b $bg_color
+        end
+        set -l separator_var canaudua_"$side"_separator_diff
+        printf '%s' $$separator_var
+      end
+    end
+
+    # Print content of item
+    set -l fg_color_var canaudua_"$item"_fg
+    set -l fg_color $$fg_color_var
+    set_color $fg_color -b $bg_color
+    printf '%s' " $output "
+    # Save info of current item to next item
+    set prev_bg_color $bg_color
+    set prev_item $item
   end
+
+  # Print head of prompt if is left prompt
+  if test "$side" = "left"
+    set_color $prev_bg_color -b normal
+    printf '%s' $canaudua_left_separator_diff
+  end
+
+  # Reset color after print prompt
+  set_color normal
 end
