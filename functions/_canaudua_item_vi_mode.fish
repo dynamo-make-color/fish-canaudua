c2057a1d88a892f0162a77aa4f067acc1bdf40cc
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-23 05:15:21 +0700

    Fix vi mode not have background

--- functions/_canaudua_item_vi_mode.fish
+++ functions/_canaudua_item_vi_mode.fish
@@ -1,19 +1,19 @@
 function _canaudua_item_vi_mode
   test "$fish_key_bindings" = "fish_vi_key_bindings"; and switch $fish_bind_mode
     case default
-      set canaudua_vi_mode_bg $canaudua_vi_mode_bg_default
+      set -g canaudua_vi_mode_bg $canaudua_vi_mode_bg_default
       set_color $canaudua_vi_mode_fg_default
       echo -n 'N'
     case insert
-      set canaudua_vi_mode_bg $canaudua_vi_mode_bg_insert
+      set -g canaudua_vi_mode_bg $canaudua_vi_mode_bg_insert
       set_color $canaudua_vi_mode_fg_insert
       echo -n 'I'
     case replace replace-one replace_one
-      set canaudua_vi_mode_bg $canaudua_vi_mode_bg_replace
+      set -g canaudua_vi_mode_bg $canaudua_vi_mode_bg_replace
       set_color $canaudua_vi_mode_fg_replace
       echo -n 'R'
     case visual
-      set canaudua_vi_mode_bg $canaudua_vi_mode_bg_visual
+      set -g canaudua_vi_mode_bg $canaudua_vi_mode_bg_visual
       set_color $canaudua_vi_mode_fg_visual
       echo -n 'V'
   end
