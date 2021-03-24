a5ded7c7fbd4e92305c2bed159f023d8300ec292
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-24 07:36:52 +0700

    Resolved #3: Asynchronous left and right prompt (not widget section)

--- conf.d/canaudua.fish
+++ conf.d/canaudua.fish
@@ -4,8 +4,8 @@ set -g canaudua_left_prompt_var canaudua_left_prompt_$fish_pid
 set -g canaudua_right_prompt_var canaudua_right_prompt_$fish_pid
 set -gx canaudua_pid $fish_pid
 
-function _canaudua_initial -e fish_prompt -v fish_bind_mode
-  _canaudua_setup &
+function _canaudua_spawn -e fish_prompt -v fish_bind_mode
+  setsid fish -c "_canaudua_setup $fish_bind_mode" &
   command kill $canaudua_last_pid 2>/dev/null
   set -g canaudua_last_pid (jobs --last --pid)
   builtin disown $canaudua_last_pid 2>/dev/null
--- functions/_canaudua_setup.fish
+++ functions/_canaudua_setup.fish
@@ -1,7 +1,13 @@
-function _canaudua_setup
-  set -g canaudua_left_prompt_$fish_pid (_canaudua_prompt)
+function _canaudua_setup -a fish_bind_mode
+  # Inherit variables from last process while async
+  set -g fish_bind_mode $fish_bind_mode
 
   for setting in items powerline color
     printf 'set -U canaudua_%s\n' (cat ~/.config/fish/functions/setting/$setting) | source
   end
+
+  set -l left_prompt (_canaudua_side_prompt left)
+  set -l right_prompt (_canaudua_side_prompt right)
+  set -U canaudua_left_prompt_$canaudua_pid $left_prompt' '
+  set -U canaudua_right_prompt_$canaudua_pid $right_prompt
 end
