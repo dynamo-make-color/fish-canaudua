1d58c2b1646d2c50235a0c0caf943e2fa2e776a2
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-24 08:21:04 +0700

    Resolved #7: Show tag or commit hash in git section

--- functions/_canaudua_item_git.fish
+++ functions/_canaudua_item_git.fish
@@ -1,5 +1,23 @@
 function _canaudua_item_git
   __fish_is_git_repository; or return
+  set -l current_name
   set -l branch (git branch --show-current 2>/dev/null); or return
-  printf '%s' $branch
+  set -l tag (git describe --tags --exact-match 2>/dev/null)
+
+  # Get current information of folder
+  git rev-parse -q --short=7 HEAD | read -lL commit
+
+  if test -n "$branch"
+    if test "$branch" != master -a "$branch" != main
+      set current_name $branch
+    else
+      set current_name ' '
+    end
+  else if test -n "$tag"
+    set current_name $tag
+  else
+    set current_name $commit
+  end
+
+  printf '%s' $current_name
 end
