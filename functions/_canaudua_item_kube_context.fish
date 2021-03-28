db40706ef088f7142411346126b191a4b75a467b
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-28 10:58:23 +0700

    Resolved #1: Shorten kube context on GKE/EKS/DOKS

--- functions/_canaudua_item_kube_context.fish
+++ functions/_canaudua_item_kube_context.fish
@@ -1,5 +1,15 @@
 function _canaudua_item_kube_context
   type -q kubectl; or return
   set -l context (kubectl config current-context 2>/dev/null)
+  if string match -qr 'gke_(?<project>.*)_(?<zone>.*)_(?<cluster>.*)' -- $context
+    # GKE cluster
+    set context $cluster
+  else if string match -qr 'arn:aws:eks:(?<zone>.*):\d+:(?<cluster>.*)' -- $context
+    # EKS cluster
+    set context $cluster
+  else if string match -qr 'do-(?<zone>.*)-(?<cluster>.*)' -- $context
+    # DOKS cluster
+    set context $cluster
+  end
   test -z $context; or printf '⎈ %s' $context
 end
