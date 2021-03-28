6406d077fb5f29d36a465185b43f7b4a8c4ec86a
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-29 00:54:26 +0700

    Fix for fish <= 3.2
    https://github.com/fish-shell/fish-shell/issues/7842

--- conf.d/canaudua.fish
+++ conf.d/canaudua.fish
@@ -5,7 +5,7 @@ set -g canaudua_right_prompt_var canaudua_right_prompt_$fish_pid
 set -gx canaudua_pid $fish_pid
 
 function _canaudua_spawn -e fish_prompt -v fish_bind_mode
-  setsid fish -c "_canaudua_setup $fish_bind_mode" &
+  fish -c "_canaudua_setup $fish_bind_mode" < /dev/null &
   command kill $canaudua_last_pid 2>/dev/null
   set -g canaudua_last_pid (jobs --last --pid)
   builtin disown $canaudua_last_pid 2>/dev/null
