66e9c16d7be024b1de4468db89be814242337189
Trần Đức Nam <dynamo.foss@gmail.com>
2021-03-29 01:10:33 +0700

    Improve status git section

--- functions/_canaudua_item_git.fish
+++ functions/_canaudua_item_git.fish
@@ -22,28 +22,28 @@ function _canaudua_item_git
 
   # Get status of repo and remote repo
   set -l info (git status --porcelain)
-  set -l staged (string match -r '^[AMDR]' $info | count)
-  set -l dirty (string match -r '^ [AMDR]' $info | count)
+  set -l staged (string match -r '^[AMDR].' $info | count)
+  set -l dirty (string match -r '^.[AMDR]' $info | count)
   set -l untracked (string match -r '^\?\?' $info | count)
   set -l conflicted (string match -r '^UU' $info | count)
   set -l stashed (git stash list | count)
   git rev-list --count --left-right @{upstream}...HEAD 2>/dev/null | read -l -d \t upstream_behind upstream_ahead
 
   # Set background of item
-  if not git diff --no-ext-diff --quiet --exit-code 2>/dev/null
+  if test $dirty -ne 0 2>/dev/null
     set -U canaudua_git_bg $canaudua_git_bg_dirty
-  else if not git diff --no-ext-diff --cached --quiet --exit-code 2>/dev/null
+  else if test $staged -ne 0 2>/dev/null
     set -U canaudua_git_bg $canaudua_git_bg_staged
   else
     set -U canaudua_git_bg $canaudua_git_bg_clean
   end
 
   printf '%s' $current_name
-  test $conflicted -ne 0; and printf ' %s %s' $canaudua_git_icon_conflicted $conflicted
-  test $staged -ne 0; and printf ' %s %s' $canaudua_git_icon_staged $staged
-  test $dirty -ne 0; and printf ' %s %s' $canaudua_git_icon_dirty $dirty
-  test $untracked -ne 0; and printf ' %s %s' $canaudua_git_icon_untracked $untracked
-  test $stashed -ne 0; and printf ' %s %s' $canaudua_git_icon_stashed $stashed
-  test $upstream_behind -ne 0; and printf ' %s %s' $canaudua_git_icon_behind $upstream_behind
-  test $upstream_ahead -ne 0; and printf ' %s %s' $canaudua_git_icon_ahead $upstream_ahead
+  test $conflicted -ne 0 2>/dev/null; and printf ' %s %s' $canaudua_git_icon_conflicted $conflicted
+  test $staged -ne 0 2>/dev/null; and printf ' %s %s' $canaudua_git_icon_staged $staged
+  test $dirty -ne 0 2>/dev/null; and printf ' %s %s' $canaudua_git_icon_dirty $dirty
+  test $untracked -ne 0 2>/dev/null; and printf ' %s %s' $canaudua_git_icon_untracked $untracked
+  test $stashed -ne 0 2>/dev/null; and printf ' %s %s' $canaudua_git_icon_stashed $stashed
+  test $upstream_behind -ne 0 2>/dev/null; and printf ' %s %s' $canaudua_git_icon_behind $upstream_behind
+  test $upstream_ahead -ne 0 2>/dev/null; and printf ' %s %s' $canaudua_git_icon_ahead $upstream_ahead
 end
