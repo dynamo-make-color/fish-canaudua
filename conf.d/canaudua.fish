fbf8619aa703eb722f1058f346ccaf76f826071e
Trần Đức Nam <dynamo.foss@gmail.com>
2021-04-14 00:34:50 +0700

    Fixed #9: Fix not show current prompt by inject into fish_prompt

--- conf.d/canaudua.fish
+++ conf.d/canaudua.fish
@@ -1,25 +1,3 @@
-status is-interactive; or exit
-
-set -g canaudua_left_prompt_var canaudua_left_prompt_$fish_pid
-set -g canaudua_right_prompt_var canaudua_right_prompt_$fish_pid
-set -gx canaudua_pid $fish_pid
-
-function _canaudua_spawn -e fish_prompt -v fish_bind_mode
-  fish -c "_canaudua_setup $fish_bind_mode" < /dev/null &
-  command kill $canaudua_last_pid 2>/dev/null
-  set -g canaudua_last_pid (jobs --last --pid)
-  builtin disown $canaudua_last_pid 2>/dev/null
-end
-
-function _canaudua_refresh -v canaudua_left_prompt_$fish_pid -v canaudua_right_prompt_$fish_pid
-  commandline -f repaint
-end
-
-function _canaudua_exit -e fish_exit
-  set -e canaudua_left_prompt_$fish_pid
-  set -e canaudua_right_prompt_$fish_pid
-end
-
 function _canaudua_uninstall -e canaudua_uninstall
   set -n | string replace -fr '(^canaudua.*)' 'set -e $1' | source
   functions -e (functions -a | string match -er '^_canaudua_')
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,3 +1,22 @@
+status is-interactive; or exit
+
+set -g canaudua_left_prompt_var canaudua_left_prompt_$fish_pid
+set -g canaudua_right_prompt_var canaudua_right_prompt_$fish_pid
+set -gx canaudua_pid $fish_pid
+
 function fish_prompt
+  fish -c "_canaudua_setup $fish_bind_mode" < /dev/null &
+  command kill $canaudua_last_pid 2>/dev/null
+  set -g canaudua_last_pid (jobs --last --pid)
+  builtin disown $canaudua_last_pid 2>/dev/null
   string unescape $$canaudua_left_prompt_var
+end
+
+function _canaudua_refresh -v $canaudua_left_prompt_var -v $canaudua_right_prompt_var
+  commandline -f repaint
+end
+
+function _canaudua_exit -e fish_exit
+  set -e canaudua_left_prompt_$fish_pid
+  set -e canaudua_right_prompt_$fish_pid
 end
