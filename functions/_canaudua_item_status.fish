ba7f2aec8a3b95a57364708da43adc31dd8c61e9
Trần Đức Nam <dynamo.foss@gmail.com>
2021-06-05 18:21:07 +0700

    Fixed #14

--- functions/_canaudua_item_status.fish
+++ functions/_canaudua_item_status.fish
@@ -1,6 +1,6 @@
 function _canaudua_item_status
   if string match -qv 0 $canaudua_last_pipestatus # If there is a failure anywhere in the pipestatus
-    if test $canaudua_last_pipestatus = 1 # If simple failure
+    if test "$canaudua_last_pipestatus" = 1 # If simple failure
       set -g canaudua_status_bg $canaudua_status_bg_failure
       set_color $canaudua_status_fg_failure
       printf '%s' $canaudua_status_icon_failure' ' 1
