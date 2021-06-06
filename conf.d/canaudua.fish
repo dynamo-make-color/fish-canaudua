d33c6db8d18b93fed8ec61dbb0f43e7f699a74eb
Trần Đức Nam <dynamo.foss@gmail.com>
2021-06-06 16:16:48 +0700

    Remove old functions and variables when install

--- conf.d/canaudua.fish
+++ conf.d/canaudua.fish
@@ -3,5 +3,9 @@ function _canaudua_uninstall -e canaudua_uninstall
   functions -e (functions -a | string match -er '^_canaudua_')
 end
 
+function _canaudua_install -e canaudua_install
+  _canaudua_uninstall
+end
+
 bind ' ' _canaudua_show_on_command
 bind -M insert ' ' _canaudua_show_on_command
