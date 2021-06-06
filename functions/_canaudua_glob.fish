97253fa14dfb6c85af084759bc1cc47fe3a4b003
Trần Đức Nam <dynamo.foss@gmail.com>
2021-06-06 17:34:57 +0700

    Set cache of language/framework/tool for folder that not adapt

--- functions/_canaudua_glob.fish
+++ functions/_canaudua_glob.fish
@@ -5,10 +5,11 @@ function _canaudua_glob -a pattern_type
   set -l pattern_var canaudua_{$pattern_type}_glob
 
   # Search in cache before
+  test "$cache_var" = 0; and return 1
   test "$cache_var" = "$mtime"; and return 0
 
   # Find with pattern
-  test -z (find . -maxdepth 1 -iregex $$pattern_var -print -quit 2>/dev/null); and return 1
+  test -z (find . -maxdepth 1 -iregex $$pattern_var -print -quit 2>/dev/null); and set -U $cache_var 0; and return 1
 
   # Set cache for glob search
   set -U $cache_var $mtime
