e1f9a677ece3d21424ab8734edfb614e02c15987
Trần Đức Nam <dynamo.foss@gmail.com>
2021-06-06 23:56:54 +0700

    Resolved #17: Show tools/languages/frameworks' version in child folder

--- functions/_canaudua_item_ruby.fish
+++ functions/_canaudua_item_ruby.fish
@@ -1,5 +1,5 @@
 function _canaudua_item_ruby
-  if _canaudua_glob ruby
+  if _canaudua_upglob ruby
     set -l ruby_version (ruby --version | awk '{print $2}')
     printf '%s' $canaudua_ruby_icon' ' $ruby_version
   end
---
+++ functions/_canaudua_upglob.fish
@@ -0,0 +1,28 @@
+function _canaudua_upglob -a pattern_type
+  set -l path .
+
+  while test (realpath $path) != "/" -a (realpath $path) != (realpath $HOME/..)
+    _canaudua_glob $pattern_type $path; and return 0
+    set path $path/..
+  end
+  return 1
+end
+
+function _canaudua_glob -a pattern_type directory
+  set -l mtime (stat -c '%Y' $directory)
+  set -l folder (realpath $directory | string escape --style=var)
+  set -l cache_var canaudua_glob_{$folder}_{$pattern_type}
+  set -l pattern_var canaudua_{$pattern_type}_glob
+
+  # Search in cache before
+  test "$cache_var" = 0; and return 1
+  test "$cache_var" = "$mtime"; and return 0
+
+  # Find with pattern
+  test -z (find (realpath $directory) -maxdepth 1 -iregex $$pattern_var -print -quit 2>/dev/null); and set -U $cache_var 0; and return 1
+
+  # Set cache for glob search
+  set -U $cache_var $mtime
+
+  return 0
+end
