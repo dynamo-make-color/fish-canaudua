eb091523eb7d858ae69ec7b65a1390401a266192
Trần Đức Nam <dynamo.foss@gmail.com>
2021-06-08 08:59:16 +0700

    Add condition of ruby command

--- functions/_canaudua_item_ruby.fish
+++ functions/_canaudua_item_ruby.fish
@@ -1,6 +1,7 @@
 function _canaudua_item_ruby
-  if _canaudua_upglob ruby
-    set -l ruby_version (ruby --version | awk '{print $2}')
-    printf '%s' $canaudua_ruby_icon' ' $ruby_version
-  end
+  type -q ruby; or return
+  _canaudua_upglob ruby; or return
+
+  set -l ruby_version (ruby --version | awk '{print $2}')
+  printf '%s' $canaudua_ruby_icon' ' $ruby_version
 end
