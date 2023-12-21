d361c6b45715bc139b3a64a6cf520f130124488e
Trần Đức Nam <me@dynamotn.dev>
2023-12-21 14:22:58 +0700

    perf(git): improve performance to show git information

--- functions/__canaudua_item_git.fish
+++ functions/__canaudua_item_git.fish
@@ -1,23 +1,42 @@
 function __canaudua_item_git
-  __fish_is_git_repository; or return
   set -l current_name
 
   # Get current information of folder
-  set -l branch (git branch --show-current 2>/dev/null)
-  set -l tag (git describe --tags --exact-match 2>/dev/null)
-  git rev-parse -q --short=7 --is-inside-work-tree HEAD | read -lL is_work_tree commit
-  test "$is_work_tree" = true; or return
-
-  if test -n "$branch"
+  if git branch --show-current 2>/dev/null | read -l branch
     if test "$branch" != master -a "$branch" != main
       set current_name $canaudua_git_icon_branch' '$branch
     else
       set current_name $canaudua_git_icon_branch
     end
-  else if test -n "$tag"
+  else if test $pipestatus[1] != 0
+    return
+  else if git describe --tags --exact-match 2>/dev/null | read -l tag
     set current_name $canaudua_git_icon_tag$tag
   else
+    git rev-parse -q --short=7 --is-inside-work-tree HEAD | read -lL is_work_tree commit
+    test "$is_work_tree" = true; or return
     set current_name $canaudua_git_icon_commit$commit
+  end
+
+  # Operation
+  if test -d .git/rebase-merge
+    test -f .git/rebase-merge/interactive && set -f operation rebase-i || set -f operation rebase-m
+  else if test -d .git/rebase-apply
+    if test -f .git/rebase-apply/rebasing
+      set -f operation rebase
+    else if test -f .git/rebase-apply/applying
+      set -f operation am
+    else
+      set -f operation am/rebase
+    end
+  else if test -f .git/MERGE_HEAD
+    set -f operation merge
+  else if test -f .git/CHERRY_PICK_HEAD
+    set -f operation cherry-pick
+  else if test -f .git/REVERT_HEAD
+    set -f operation revert
+  else if test -f .git/BISECT_LOG
+    set -f operation bisect
   end
 
   # Get status of repo and remote repo
@@ -46,4 +65,5 @@ function __canaudua_item_git
   test $stashed -ne 0 2>/dev/null; and printf ' %s %s' $canaudua_git_icon_stashed $stashed
   test $upstream_behind -ne 0 2>/dev/null; and printf ' %s %s' $canaudua_git_icon_behind $upstream_behind
   test $upstream_ahead -ne 0 2>/dev/null; and printf ' %s %s' $canaudua_git_icon_ahead $upstream_ahead
+  test -n "$operation"; and printf ' %s' $operation
 end
