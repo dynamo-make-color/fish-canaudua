e9dc8b39a99e5d5be17019c005774527a4dd7a72
Trần Đức Nam <me@dynamotn.dev>
2023-12-21 13:39:17 +0700

    perf(gcloud): use config file instead of command

--- functions/__canaudua_item_gcloud.fish
+++ functions/__canaudua_item_gcloud.fish
@@ -1,8 +1,12 @@
 function __canaudua_item_gcloud
   type -q gcloud; or return
   if set -q canaudua_show_gcloud
-    set -l account (gcloud config get-value account)
-    set -l project (gcloud config get-value project)
-    test -z $account; or printf '%s' $canaudua_gcloud_icon' ' $account': '$project
+    set -q CLOUDSDK_CONFIG || set -l CLOUDSDK_CONFIG ~/.config/gcloud
+    path is $CLOUDSDK_CONFIG/active_config
+      and read -l config <$CLOUDSDK_CONFIG/active_config
+      and path is $CLOUDSDK_CONFIG/configurations/config_$config
+      and string match -qr '^\s*project\s*=\s*(?<project>.*)' <$CLOUDSDK_CONFIG/configurations/config_$config
+      and string match -qr '^\s*account\s*=\s*(?<account>.*)' <$CLOUDSDK_CONFIG/configurations/config_$config
+      and printf '%s' $canaudua_gcloud_icon' ' $account': '$project
   end
 end
